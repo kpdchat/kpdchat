a140813ed46c7eda62ffc439116445b847109380
Vlasov <bboysmo3@gmail.com>
2023-07-28 13:53:02 +0300

    modification file package.json and fixed bags

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './App';
 
@@ -7,4 +7,3 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
      <App />
 );
-
