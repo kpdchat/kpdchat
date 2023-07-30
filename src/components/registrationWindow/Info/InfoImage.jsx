75aff0f2d40e678f1cd822e0bfebfc7822264996
Vlasov <bboysmo3@gmail.com>
2023-07-30 15:28:43 +0300

    End of registration window layout, modification of style files, modules

---
+++ src/components/registrationWindow/Info/InfoImage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import img from "../../images/RegistrationWindow/puzzles_img.png";
+
+export function InfoImage() {
+  return (
+      <div className='info_img'>
+        <img src ={img} alt = ""/>
+      </div>
+  );
+}
