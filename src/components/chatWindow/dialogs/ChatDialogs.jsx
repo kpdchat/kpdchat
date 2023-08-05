cab2b3187e06e774362bae5f2720cc1a2f15ee10
mtjal <140608286+mtjal@users.noreply.github.com>
2023-08-05 12:57:37 +0300

    Applied VS Code auto formatting to most files (#6)

--- src/components/chatWindow/dialogs/ChatDialogs.jsx
+++ src/components/chatWindow/dialogs/ChatDialogs.jsx
@@ -2,20 +2,20 @@ import { ReactComponent as Logo } from '../../../images/chat-window/logo.svg'
 import DialogItem from './DialogItem'
 import DialogSearch from './DialogSearch'
 export default function ChatDialogs() {
-    return (
-        <section className='chat__dialogs dialogs'>
-          <div className='dialogs__logo'>
-            <Logo />
-            <h1> <span>kpd</span>Chat</h1>
-          </div>
-          <DialogSearch/>
-          <div className='dialogs__list list scroll-bar'>
-            <div className="list__container ">
-              <DialogItem/>
-            </div>
+  return (
+    <section className='chat__dialogs dialogs'>
+      <div className='dialogs__logo'>
+        <Logo />
+        <h1> <span>kpd</span>Chat</h1>
+      </div>
+      <DialogSearch />
+      <div className='dialogs__list list scroll-bar'>
+        <div className="list__container ">
+          <DialogItem />
+        </div>
 
-          </div>
-          <span></span>
-        </section>
-    )
+      </div>
+      <span></span>
+    </section>
+  )
 }
--- src/components/registrationWindow/RegistrationWindow.jsx
+++ src/components/registrationWindow/RegistrationWindow.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Info} from "./Info/Info";
-import {Registration} from "./Registration/Registration";
+import { Info } from "./Info/Info";
+import { Registration } from "./Registration/Registration";
 
-export function RegistrationWindow({onDisplay, isActive}) {
+export function RegistrationWindow({ onDisplay, isActive }) {
 
 
   return (
-        <div className= {isActive ? 'display-none' : 'registration_window'}>
+    <div className={isActive ? 'display-none' : 'registration_window'}>
 
-          <Info />
-          <Registration onDisplay={onDisplay} />
+      <Info />
+      <Registration onDisplay={onDisplay} />
 
-        </div>
+    </div>
   );
 }
