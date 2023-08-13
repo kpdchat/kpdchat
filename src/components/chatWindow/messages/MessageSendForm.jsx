1c2fe35bd1186a4f8f68ca4121557eeeea2f5cc3
Yelyzaveta Berest <lizaberest97@gmail.com>
2023-08-13 17:38:02 +0300

    create focus for search and input messages, add resizing of input message

--- src/components/chatWindow/messages/MessageSendForm.jsx
+++ src/components/chatWindow/messages/MessageSendForm.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 export default function MessageSendForm() {
+
+    function onTextareaInput(e) {
+        e.target.style.height = 'auto'
+        e.target.style.height = e.target.scrollHeight + 2 + "px"
+    }
     return (
         <form action="">
-            <input className='text-16' type="text" placeholder='Написати...' />
-            <label>
+            <textarea rows="1" onInput={onTextareaInput} className='text-16 scroll-bar' type="text" placeholder='Написати...'/>
+            {/* <label>
                 <input type="file" />
-            </label>
+            </label> */}
             <button type='submit'></button>
         </form>
     )
