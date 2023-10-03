042c52faa31d0908e857829043ceb8491459cb8a
Vlasov <bboysmo3@gmail.com>
2023-10-03 14:34:07 +0300

    corrected info about us

--- src/components/chatWindow/navigation/nav-modal/NavInfoModal.jsx
+++ src/components/chatWindow/navigation/nav-modal/NavInfoModal.jsx
@@ -1,42 +1,49 @@
-import React from "react";
-import close_icon from "../../../../images/chat-window/close.png"
-import info_icon from "../../../../images/chat-window/info.png"
+import React from 'react';
+import {useDispatch} from 'react-redux';
+import {setModalClose} from '../../../../store/actions/uiActions';
+import {MdOutlineClose} from 'react-icons/md';
 
-export default function NavInfoModal({ isOpen, setOpen }) {
+export default function NavInfoModal() {
+    const dispatch = useDispatch();
 
     function onCloseClick() {
-        setOpen(!isOpen)
-    }
-
-    function onContentClick(e) {
-        e.stopPropagation()
+        dispatch(setModalClose());
     }
 
     return (
-        <div className="settings__info-about modal-container info-about" onClick={onCloseClick} >
-            <div className="info-about__content" onClick={onContentClick}>
-                <div className="modal-title">
-                    <h2>Про KPD Chat</h2>
-                    <img className="cursor-pointer" src={close_icon} alt="" onClick={onCloseClick}/>
-                </div>
-                <div className="info-about__text">
-                    <div className="info-about__about">
-                        <img src={info_icon} alt="" />
-                        <h4 className="text-inter-16-400">
+        <div className='settings__info-about modal-container info-about'>
+            <div className='info-about__content'>
+                <div className='info-about__text text'>
+                    <div className='text__title'>
+                        <h2 className='text-inter-18-600'>
                             Особливості KPD Chat
-                        </h4>
+                        </h2>
+                        <MdOutlineClose
+                            className='cursor-pointer'
+                            size='24'
+                            onClick={ onCloseClick }
+                        />
                     </div>
-                    <p className="text-inter-16-400">
-                        KPD Chat - веб додаток де будь-хто може приєднатися до обговорення різних тем, використовуючи лише ім’я користувача(без авторизації). В додатку ви можете спілкуватися більше ніж з двома користувачами, надсилати їм повідомлення, відповідати на їх повідомлення.
-                    </p>
-                    <br />
-                    <p className="text-inter-16-400">Основні функції:</p>
 
-                    <ul className="text-inter-16-400">
-                        <li>забезпечує швидший час відповіді та покращує взаємодію користувачів між собою.</li>
-                        <li> чат привчає людей проявляти емпатію, надсилання шкидливих і образливих повідомлень не вітається.</li>
-                        <li>Кімнати чатів: онлайн-форуми або платформи, де користувачі можуть брати участь у групових дискусіях на певні теми чи спільні інтереси</li>
-                    </ul>
+                    <div className='text__description'>
+                        <p className='text-inter-14-400'>
+                            KPD Chat - веб додаток де будь-хто може приєднатися до обговорення різних тем,
+                            використовуючи лише ім’я користувача(без авторизації). В додатку ви можете спілкуватися
+                            більше ніж з двома користувачами, надсилати їм повідомлення, відповідати на їх повідомлення.
+                        </p>
+
+                        <p className='text-inter-14-400 mt-16px'>Основні функції:</p>
+
+                        <ul className='text-inter-14-400'>
+                            <li>забезпечує швидший час відповіді та покращує взаємодію користувачів між собою.</li>
+                            <li> чат привчає людей проявляти емпатію, надсилання шкидливих і образливих повідомлень не
+                                вітається.
+                            </li>
+                            <li>Кімнати чатів: онлайн-форуми або платформи, де користувачі можуть брати участь у
+                                групових дискусіях на певні теми чи спільні інтереси
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
