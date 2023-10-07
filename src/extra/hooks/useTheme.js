6c41b02f4ddecf7a6eb420f0e74797998452e494
Vlasov <bboysmo3@gmail.com>
2023-10-07 11:49:09 +0300

    synchronization of the site theme and the users device, translated the AboutUs window

--- src/extra/hooks/useTheme.js
+++ src/extra/hooks/useTheme.js
@@ -1,10 +1,14 @@
 import {useLayoutEffect, useState} from 'react';
 
+const isDarkTheme = window?.matchMedia('(prefers-color-scheme: dark)').matches;
+const defaultTheme = isDarkTheme ? 'dark' : 'light';
+
 export default function useTheme() {
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || defaultTheme);
 
     useLayoutEffect( () => {
-        document.documentElement.setAttribute('data-theme', theme)
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
     }, [theme])
 
     return {
